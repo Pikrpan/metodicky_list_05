c519d9a3d0d13017007fc3ea44084de8eecca634
Pikrpan <54728861+Pikrpan@users.noreply.github.com>
2024-04-02 11:26:54 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+//Zde vytvoř proměnné jmeno a prijmeni
+
+
+
+
+//Zde vytvoř funkci  s názvem zadejJmeno
+//V těle funkce zadejJmeno přiřaď proměnné jmeno hodnotu prompt() 
+
+
+
+
+//Zde vytvoř funkci s názvem zadejPrijmeni
+//V těle funkce zadejPrijmeni přiřaď proměnné prijmeni hodnotu prompt()
+
+
+
+
+//Zde vytvoř funkci s názvem vypisCeleJmeno
+//V těle funkce vypisCeleJmeno vypiš hodnoty jmeno a prijmeni pomocí příkazu alert() 
+
+
+
+
